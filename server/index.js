642973095b2eb1056de37aa517ce52118c33999a
rnoelle <reidnoelle2@gmail.com>
2016-08-26 12:08:07 -0600

    switched to Digital Ocean

--- server/index.js
+++ server/index.js
@@ -146,5 +146,5 @@ app.post('/submissions/decision/:id/:acceptance', controller.acceptOrDenySubmiss
 app.delete('/cart/:id', controller.removeFromCart);
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
