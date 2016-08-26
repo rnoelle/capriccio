e0569d0b50d4cfc0567380aa8c1db0a22fd7449b
rnoelle <reidnoelle2@gmail.com>
2016-08-26 08:51:43 -0600

    I'm over the landing page. About to try hosting.

--- public/js/animations.js
+++ public/js/animations.js
@@ -2,11 +2,11 @@ $(window).scroll(function () {
   var eTop = $(this).scrollTop();
   var wTop = $(window).height();
   if (eTop > $('.three-box-three').offset().top - ($(window).height()/1.5)) {
-    $('.three-box').removeClass('hidden-left hidden-right');
+    $('.three-box').removeClass('hidden-left hidden-right hidden-fade');
     $('.three-arrow').removeClass('hidden-left hidden-right');
     }
     console.log($('#slide-in').offset().left);
-  if ($('#slide-in').offset().left > ($(window).width()/1.75)) {
+  if ($('#slide-in').offset().left > ($(window).width()/1.7)) {
     console.log('yup');
     $('#slide-in').css({
         'transform': 'translate(-'+ eTop /20 +'%, 0px)'
