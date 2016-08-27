729ae31cd49a9fec967e49d6140f49b5132d77fe
rnoelle <reidnoelle2@gmail.com>
2016-08-27 10:46:15 -0600

    redirect on server

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -81,7 +81,7 @@ module.exports = {
           res.status(303).send('unauthorized')
           return;
         } else {
-          res.status(200);
+          res.redirect('publish');
         }
       })
     }
