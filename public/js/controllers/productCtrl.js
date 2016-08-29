e49a6bf21f9ef814042c6688cd599e497459ea74
rnoelle <reidnoelle2@gmail.com>
2016-08-29 01:34:29 -0600

    fixing look/layout of product page

--- public/js/controllers/productCtrl.js
+++ public/js/controllers/productCtrl.js
@@ -3,7 +3,10 @@ angular.module('capriccio')
     var thisProduct = $stateParams.id
     function getProduct(id) {
       dataService.getProduct(id).then(function (response) {
+        var item = response;
         $scope.product = response;
+        $scope.product.scope_cover_url = item.cover_url.slice(10);
+        $scope.product.scope_score_url = item.score_url.slice(10);
       })
     }
     getProduct(thisProduct);
