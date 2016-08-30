144f4fdb7d8231f1273377b9276bd800fe479249
rnoelle <reidnoelle2@gmail.com>
2016-08-29 21:22:53 -0600

    fixed star jquery

--- public/js/directives/reviewStars.js
+++ public/js/directives/reviewStars.js
@@ -9,11 +9,7 @@ angular.module('capriccio')
       },
       link: function (scope, element, attrs) {
         if (scope.disabled === true) {
-          $('#rating5').attr("disabled", true);
-          $('#rating4').attr("disabled", true);
-          $('#rating3').attr("disabled", true);
-          $('#rating2').attr("disabled", true);
-          $('#rating1').attr("disabled", true);
+          $('#review-card').find('input').attr("disabled", true);
         }
       },
       controller: function ($scope) {
