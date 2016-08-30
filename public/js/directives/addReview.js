8104d1f31c4ee9b286d426ca711793624f207817
rnoelle <reidnoelle2@gmail.com>
2016-08-29 20:55:02 -0600

    try that

--- public/js/directives/addReview.js
+++ public/js/directives/addReview.js
@@ -1,37 +1,37 @@
 angular.module('capriccio')
-  .directive('addReview', function ($stateParams, $state, dataService) {
-    return {
-      restrict: 'E',
-      templateUrl: 'js/views/directives/addReview.html',
-      link: function (scope, element, attrs) {
-        $('#add-review-button').click(function () {
-          $('.add-review-form').removeClass('hidden');
-        })
+    .directive('addReview', function($stateParams, $state, dataService) {
+        return {
+            restrict: 'E',
+            templateUrl: 'js/views/directives/addReview.html',
+            link: function(scope, element, attrs) {
+                $('#add-review-button').click(function() {
+                    $('.add-review-form').removeClass('hidden');
+                })
 
-      },
-      controller: function ($scope, $stateParams, $state, dataService) {
-        $scope.review = {};
+            },
+            controller: function($scope, $stateParams, $state, dataService) {
+                $scope.review = {};
 
-        $scope.addReview = function() {
-          dataService.getAuth().catch((err) => {
-              console.log("Unauthorized: ", err);
-              if (err.status === 401) {
-                  $state.go('login.login')
-                } else {
-              dataService.addReview($stateParams.id, $scope.review).then(function (response) {
-                $('.review-box').prepend(`<div class="review-card">
-                  <star-rating disabled="true" value="${response.rating}"></star-rating>
-                  <h4>You said:</h4>
-                  <p>
-                    ${response.review}
-                  </p>
-                </div>`)
-              });
+                $scope.addReview = function() {
+                    dataService.getAuth().catch((err) => {
+                        console.log("Unauthorized: ", err);
+                        if (err.status === 401) {
+                            $state.go('login.login')
+                        } else {
+                            dataService.addReview($stateParams.id, $scope.review).then(function(response) {
+                                $('.review-box').prepend(`<div class="review-card">
+                                  <star-rating disabled="true" value="${response.rating}"></star-rating>
+                                  <h4>You said:</h4>
+                                  <p>
+                                    ${response.review}
+                                  </p>
+                                </div>`);
+                            });
+                        }
+                    })
+                }
+
             }
-          }
-        )
-          })
         }
-      }
-    }
-  })
+
+    })
