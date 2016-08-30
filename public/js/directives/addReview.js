92f9715d91324c3443e49acb793abfcfb6ed9350
rnoelle <reidnoelle2@gmail.com>
2016-08-29 21:15:36 -0600

    please review!

--- public/js/directives/addReview.js
+++ public/js/directives/addReview.js
@@ -11,6 +11,7 @@ angular.module('capriccio')
             },
             controller: function($scope, $stateParams, $state, dataService) {
                 $scope.review = {};
+                $scope.review.rating = "";
 
                 $scope.addReview = function() {
                     dataService.getAuth().catch((err) => {
