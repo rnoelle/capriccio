5c1da198231a492963a3ea4ba4678879ae8d9a54
rnoelle <reidnoelle2@gmail.com>
2017-08-03 13:16:08 -0600

    fixed styling on product page

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -3,14 +3,16 @@ angular.module('capriccio')
     $scope.random = function() {
         return 0.5 - Math.random();
     }
-    
+
     $scope.mainFilter = {};
     function getProducts() {
       dataService.getProducts().then(function (response) {
         var items = response;
+        console.dir(items)
         for (var i = 0; i < items.length; i++) {
           items[i].scope_cover_url = items[i].cover_url.slice(10);
         }
+        console.dir(items)
         $scope.items = items;
       })
     }
